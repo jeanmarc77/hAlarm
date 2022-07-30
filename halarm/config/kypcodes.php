bb76bdcf8674a5e8336c947e39a663dbd57ef7b7
Jean-Marc Louviaux <jeanmarc.louviaux@gmail.com>
2022-07-30 17:22:52 +0200

    Update kypcodes.php

--- halarm/config/kypcodes.php
+++ halarm/config/kypcodes.php
@@ -1,12 +1,13 @@
 <?php
 // Save as utf-8 encoding
+// Set keypad(s) unlock code(s) and user name(s)
 if (!defined('checkaccess')) {die('Direct access not permitted');}
 
 // Keypad(s) code(s)
 $KEYB[0] = '0000';
 $KEYB[1] = '0123';
 $KEYB[2] = '4567';
-// User name
+// User(s) name
 $KEYU[0] = 'Invité';
 $KEYU[1] = 'JM';
 $KEYU[2] = 'Juju';
