eaf703e99b860a6758af6b528186f9719a61f2ab
Jean-Marc Louviaux <jeanmarc.louviaux@gmail.com>
2023-06-04 12:02:33 +0200

    Create lang (English).php

---
+++ halarm/config/lang (English).php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * /srv/http/halarm/admin/config/lang.php
+ *
+ * @package default
+ */
+
+
+// Save as utf-8 encoding
+if (!defined('checkaccess')) {die('Direct access not permitted');}
+
+// Index
+$lgSTAT='Status';
+$lgOFF='Off';
+$lgHISTO='History';
+$lgBACK='Back';
+$lgKEYB='Keypad';
+$lgFULLS='Full screen';
+$lgWAIT='Wait';
+// Keyb
+$lgTRY='Try';
+$lgCHK='Check';
+$lgARM='Arming in';
+$lgSEC='secondes';
+$lgLOCK='Lock';
+$lgULOCK='Unlock';
+$lgSCEN='Senario';
+// Alarm
+$lgSTART='Starting';
+$lgUNDS='Under surveillance';
+$lgCONL='Lost connection with the central';
+$lgENTR='Entering';
+$lgLEAV='Leave';
+$lgALRM='Alarm';
+$lgATOF='Auto off alarm';
+$lgDTAM='Tamper default';
+$lg5T='5min test';
+
+?>
